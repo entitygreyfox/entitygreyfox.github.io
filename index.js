04b64903755c4934f601bec1412d9a293a03698c
ENTITY_GREY_FOX <210228463+entitygreyfox@users.noreply.github.com>
2025-08-06 21:29:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 document.getElementById("btnGIS").addEventListener("click", () => {
-
   let sitepage = document.getElementById("searchatext").value;
   
   let searchatext = `entitygreyfox.github.io/${sitepage}`;  
