0cb9061ccf77638956bd5b84294654fbe02efe7f
ENTITY_GREY_FOX <210228463+entitygreyfox@users.noreply.github.com>
2025-08-06 21:14:18 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("btnGIS").addEventListener("click", () => {
+
+  let sitepage = document.getElementById("search-text").value;
+  
+  let search-text = `entitygreyfox.github.io/${sitepage}`;  
+});
